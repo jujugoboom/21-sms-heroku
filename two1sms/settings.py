02bda4126ec5b3fb2033f7e5d6d9e9fe44d54ac6
jujugoboom <jujugoboom@gmail.com>
2016-10-23 20:48:43 -0400

    Add databaseless test runner

--- two1sms/settings.py
+++ two1sms/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 import os
 import dotenv
 from two1.wallet import Two1Wallet
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -84,6 +85,14 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'two1sms.wsgi.application'
 
+
+# Database
+# https://docs.djangoproject.com/en/1.9/ref/settings/#databases
+
+DATABASES = {}
+DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+
+
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
 
@@ -121,3 +130,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+TEST_RUNNER = 'testing.DatabaselessTestRunner'
