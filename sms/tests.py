5b930f23b6ffd36eaa3c9f90f8133cac353843a3
jujugoboom <jujugoboom@gmail.com>
2016-10-23 20:30:11 -0400

    Start adding tests

--- sms/tests.py
+++ sms/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from sms import views
+from django.test.client import RequestFactory
 
 # Create your tests here.
+class SmsTestCase(TestCase):
+    rf = RequestFactory()
+    def testStartSuccess():
+        request = rf.post("/buy/", "{\"phone\": \"410-349-7954\", \"text\": \"This is a successful test\"}")
+        self.assertEqual(request.status_code, 402)
